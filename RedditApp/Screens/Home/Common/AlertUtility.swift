f456dd003f4d21c68b025e74051e33110928d136
Rajesh Ghosh <ghosh.professional@gmail.com>
2021-12-29 12:57:07 -0500

    [RG] Create reusable alert view to display error.

--- RedditApp/Screens/Home/Common/AlertUtility.swift
+++ RedditApp/Screens/Home/Common/AlertUtility.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+class AlertUtility {
+    
+    typealias CompletionHandler = ((UIAlertAction, Int) -> (Void))
+    
+    private static func createActionController(_ prerredStyle: UIAlertController.Style, title: String?, message: String?, buttonTitles: [String], completionHandler: CompletionHandler?) -> UIAlertController {
+        
+            let title = title ?? NSLocalizedString("", comment: "")
+            let alert = UIAlertController(title: title, message: message, preferredStyle: prerredStyle)
+        
+        //Add buttons to the alert
+        for (index, buttonTitle) in buttonTitles.enumerated() {
+            let action = UIAlertAction(title: buttonTitle, style: .default, handler: {(action) in
+                completionHandler?(action, index)
+            })
+            alert.addAction(action)
+        }
+        
+        return alert
+    }
+    
+    public static func showAlert(_ viewController: UIViewController, title: String?, message: String?, buttonTitles:[String], completionHandler: CompletionHandler?) {
+        
+        let alert = createActionController(.alert, title: title, message: message, buttonTitles: buttonTitles, completionHandler: completionHandler)
+        
+        viewController.present(alert, animated: true, completion: nil)
+    }
+}
